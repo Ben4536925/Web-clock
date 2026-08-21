7397747798ddff6cadc8cdce6a3ac7b5b1b975fd
Ben4536925 <kaito10032013@gmail.com>
2026-08-21 10:58:13 +0900

    完成した

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var x = 0;
-const intervalID = setInterval(refreshclock, 10);
+const intervalID = setInterval(refreshclock, 1000);
 let s = 0;
+let start_time;
+let stopwatch_intervalID;
+let stopwatch_time_ms;
+let stopwatch_time_ss;
+let stopwatch_time_raw = 0;
 //const dateObjectName = new Date([parameters]);
 let fullscreen_toggle = false;
 const root = document.documentElement;
@@ -13,10 +18,10 @@ if (color == null) {
     color = 0;
 }
 
-    document.getElementById("setting,1").value = fontsize;
-    document.getElementById("setting,2").value = color;
+document.getElementById("setting,1").value = fontsize;
+document.getElementById("setting,2").value = color;
 
-    apply_settings();
+apply_settings();
 //let fontsize = document.getElementById("setting,1").value;
 
 /*document.getElementById("setting,2").addEventListener("input", changefontsize);
@@ -49,11 +54,11 @@ function fullscreen() {
     //let  a = dateObjectName
     if (!document.fullscreenElement) {
         document.body.requestFullscreen();
-        fullscreen_button.innerHTML = "fullscreen_exit"
+        fullscreen_button.textContent = "fullscreen_exit"
         document.getElementById("tabs").style.display = "none";
     } else {
         document.exitFullscreen();
-        fullscreen_button.innerHTML = "fullscreen"
+        fullscreen_button.textContent = "fullscreen"
         document.getElementById("tabs").style.display = null;
     }
 }
@@ -69,16 +74,16 @@ function refreshclock() {
     mm = mm.padStart(2, "0");
     ss = ss.padStart(2, "0");
     let activetab = document.getElementById("tabs").activeindex;
-    time.innerHTML = HH + ":" + mm + ":" + ss;
-    //time.innerHTML = activetab;
-    date.innerHTML = now.toLocaleDateString('ja-JP');
+    document.getElementById("time").textContent = HH + ":" + mm + ":" + ss;
+    //time.textContent = activetab;
+    document.getElementById("date").textContent = now.toLocaleDateString('ja-JP');
 
     if (document.fullscreenElement) {
 
-        fullscreen_button.innerHTML = "fullscreen_exit"
+        fullscreen_button.textContent = "fullscreen_exit"
         document.getElementById("tabs").style.display = "none";
     } else {
-        fullscreen_button.innerHTML = "fullscreen"
+        fullscreen_button.textContent = "fullscreen"
         document.getElementById("tabs").style.display = null;
     }
 }
@@ -106,4 +111,55 @@ function hideAll() {
     document.getElementById("home").style.display = "none";
     document.getElementById("stopwatch").style.display = "none";
     document.getElementById("settings").style.display = "none";
+}
+
+function start_stop_stopwatch() {
+    if (stopwatch_intervalID) {
+        stop_stopwatch();
+    } else {
+        start_stopwatch();
+    }
+}
+
+function start_stopwatch() {
+    let now = new Date();
+    start_time = now - stopwatch_time_raw;
+    stopwatch_intervalID = setInterval(refresh_stopwatch, 10);
+    document.getElementById("play-button-text").textContent = "一時停止";
+    document.getElementById("play-button").textContent = "pause";
+}
+
+function stop_stopwatch() {
+    clearInterval(stopwatch_intervalID);
+    stopwatch_intervalID = null;
+    document.getElementById("play-button-text").textContent = "再開";
+    document.getElementById("play-button").textContent = "play_arrow";
+}
+
+function refresh_stopwatch() {
+    stopwatch_time_raw = new Date() - start_time;
+
+    stopwatch_time_ss = Math.floor(stopwatch_time_raw / 1000);
+    stopwatch_time_ss = stopwatch_time_ss % 60;
+    stopwatch_time_ss = String(stopwatch_time_ss);
+    stopwatch_time_ss = stopwatch_time_ss.padStart(2, "0");
+
+    stopwatch_time_mm = Math.floor(stopwatch_time_raw / 60000);
+    stopwatch_time_mm = String(stopwatch_time_mm);
+
+    stopwatch_time_ms = Math.floor(stopwatch_time_raw / 10);
+    stopwatch_time_ms = stopwatch_time_ms % 100;
+    stopwatch_time_ms = String(stopwatch_time_ms);
+    stopwatch_time_ms = stopwatch_time_ms.padStart(2, "0");
+
+    document.getElementById("stopwatch_time").textContent = stopwatch_time_mm + ":" + stopwatch_time_ss;
+    document.getElementById("stopwatch_ms").textContent = "." + stopwatch_time_ms;
+}
+
+function reset_stopwatch() {
+    stopwatch_time_raw = 0;
+    stop_stopwatch();
+    document.getElementById("play-button-text").textContent = "開始";
+    document.getElementById("stopwatch_time").textContent = "0:00";
+    document.getElementById("stopwatch_ms").textContent = ".00";
 }
